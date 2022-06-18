a0c82b80e00448f398d02f75a3bdf5ed7108e73a
rawrencen <38336011+rawrencen@users.noreply.github.com>
2022-06-17 21:52:08 -0400

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+// const myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello world!';
+
+let myImage = document.querySelector('img');
+myImage.onclick = () => {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/image1.jpeg') {
+        myImage.setAttribute('src','images/image2.jpeg');
+    }
+    else {
+        myImage.setAttribute('src','images/image1.jpeg')
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+        setUserName();
+    }
+    else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = myName + " is cool";
+    }
+  }
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} 
+else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = storedName + ' is cool';
+}
+
+myButton.onclick = () => {
+    setUserName();
+} 
+
